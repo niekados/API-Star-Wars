16b2159888b0f915b153dd04392dc1ae380383ce
niekados <tiny.post@yahoo.com>
2021-07-25 20:27:43 +0000

    third

--- main.js
+++ main.js
@@ -14,15 +14,36 @@ function getData(type, cb) {
     xhr.send();
 }
 
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key){
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+
+    return `<tr>${tableHeaders}</tr>`
+}
+
 function writeToDocument(type) {
+    var tableRows = [];
     var el = document.getElementById("data");
     el.innerHTML = "";
 
     getData(type, function(data) {
         data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item) {
-            el.innerHTML += "<p>" + item.name + "</p>";
+            var dataRow = [];
+            Object.keys(item).forEach(function(key){
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15)
+                dataRow.push(`<td>${truncatedData}</td>`);
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`);
+
         });
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`
+
     });
 }
